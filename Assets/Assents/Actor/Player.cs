4ac4d38319cad0a13c4f14a9112dd1683e4dbdeb
Richard Greaves <roboritch@gmail.com>
2017-03-07 18:37:05 -0800

    work on the player class

--- Assets/Assents/Actor/Player.cs
+++ Assets/Assents/Actor/Player.cs
@@ -4,9 +4,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+	#region health
+	[SerializeField] private float maxHealth = 100;
+	[SerializeField] private float minHealth = 0; //some cards may set this > 0 for a short time
+	[SerializeField] private float health;
+	
+	private void healthInit() {
+		health = maxHealth;
+	}
+	#endregion
+
+	#region movment atachment
+	#endregion
+
+	#region over time efects
+		
+	#endregion
+
 	// Use this for initialization
 	void Start () {
-		
+		healthInit();
 	}
 	
 	// Update is called once per frame
