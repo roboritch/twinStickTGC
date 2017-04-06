23237a4c0cc46c1efce99c7c74b1931a24bb94a1
Richard Greaves <roboritch@gmail.com>
2017-04-06 10:20:26 -0700

    moved deck and hand to main assets folder

---
+++ Assets/Assents/Level/Enamy Spawners/EnamySpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnamySpawner : MonoBehaviour {
+	[SerializeField]
+	protected GameObject enamyPrefab;
+	//TODO load premade enamy decks from xml
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
